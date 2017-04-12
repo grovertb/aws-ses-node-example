9efba6d082ccf24e1e0792cf10b3b633bb60c26a
GroverTB <grover.jtb@gmail.com>
2017-04-12 17:52:43 -0500

    [INIT]

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+var aws = require("aws-sdk");
+var ses = new aws.SES({
+            "accessKeyId": "YOUR_ACCESS_KEY_ID",
+            "secretAccessKey": "YOUR_SECRET_ACCESS_KEY",
+            "region": "YOUR_REGION_SERVICE"
+          });
+
+var NomCuenta     = "Your Name";
+var asunto        = "Asunto SES Example";
+var email         = "recipe@domain.com";
+var body          = "<h1>TITULO</h1><br/>Grover Trujillo";
+var emailadjunto  = "";
+
+var rawMessage = `From: "Tech, Services"<yourmail@domain.com>\nTo: ${NomCuenta} <${email}>\nSubject:${asunto}\nMIME-Version: 1.0\nContent-type: Multipart/Mixed; boundary=\"NextPart\"\n\n--NextPart\nContent-Type: text/html;charset=UTF-8\n\n${body}\n\n--NextPart${emailadjunto}`;
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/send', function(req, res, next) {
+  ses.sendRawEmail({RawMessage : { Data: rawMessage } }, function(err, data) {
+    console.log('Error', err);
+    res.json(data);
+  });
+})
+
+module.exports = router;
